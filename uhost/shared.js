a8822bd5cf7530ea9c9690f6154ff14d37ac319e
Guilherme Silva <guilherme.silva@equusoft.com>
2024-06-15 15:04:37 +0100

    add animations and transformations

--- uhost/shared.js
+++ uhost/shared.js
@@ -4,13 +4,17 @@ var modal = document.querySelector('.modal');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 var modalNoButton = document.querySelector('.modal__action--negative');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
         // backdrop.style.display = 'block';
         // modal.style.display = 'block';
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(function() {
+            backdrop.classList.add('open');
+        }, 10);
     });
 }
 
@@ -31,12 +35,29 @@ function closeModal() {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 200);
 }
 
 toggleButton.addEventListener('click', function() {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'none';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
 });
 
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration', event);
+});
